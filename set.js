96d7cc8d32a558c846f6325185ac9a9cced7645f
brian254bot <ahaobrian486@gmail.com>
2025-07-06 01:29:23 +0300

    Update set.js

--- set.js
+++ set.js
@@ -14,8 +14,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "254799056874",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Pkdriller01",              
+    OWNER_NAME: process.env.OWNER_NAME || "254112941616",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " brian254boy",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'NEXUS-AI',
